033f62b42a00935486d60c75c5196126a90912cc
unit-2_Program <100725778+mehfoozkhangithub@users.noreply.github.com>
2022-12-27 10:13:05 +0530

    code chane for test case

--- CP/masai-ops-487-21-KVuMJW-797b5c0e4c261ffb948c6d8b61f651e18434a0c4/masai-ops-487-21-KVuMJW-797b5c0e4c261ffb948c6d8b61f651e18434a0c4/src/jokesApp.js
+++ CP/masai-ops-487-21-KVuMJW-797b5c0e4c261ffb948c6d8b61f651e18434a0c4/masai-ops-487-21-KVuMJW-797b5c0e4c261ffb948c6d8b61f651e18434a0c4/src/jokesApp.js
@@ -3,50 +3,53 @@
 
 function setJokes(data) {
   // get the joke-container div
-  document.getElementById("joke-container").innerHTML=null;
-  let div=document.getElementById("joke-container");
-  // craete a div and add a p tag in it with class as joke-text
-  // console.log(data)
-
-    let div1=document.createElement("div");
-    div1.setAttribute("class", "joke-text");
-    let p=document.createElement("p");
-    p.innerText=data.value;
-
-    div1.append(p)
-    div.append(div1)
+  
+    document.getElementById("joke-container").innerHTML=null;
+    let div=document.getElementById("joke-container");
+    // craete a div and add a p tag in it with class as joke-text
+    // console.log(data)
+  
+      let div1=document.createElement("div");
+      let p=document.createElement("p");
+      p.setAttribute("class", "joke-text");
+      p.textContent=data.value;
+  
+      div1.append(p)
+      div.append(div1)
+  
+   
+    
+  }
 
   
 
   // append the div to joke-container div
-}
+
 
 let getRandomJoke = async () => {
   // fetch request to get a random joke
   try {
     let res= await fetch(`https://api.chucknorris.io/jokes/random`)
     let data= await res.json()
-    setJokes(data)
+    return data
   } catch (error) {
-    console.log(error);
+    return "something went wrong";;
   }
   // return the data on a successfull request
   // return error text on failure
 };
-getRandomJoke()
 
 
 let getJokeByCategory = async (event) => {
-  event.preventDefault();
-  let info=document.getElementById("get-category").value
-  // console.log(info)
+  
   //  fetch request to get a random joke by category
+  let category_name=event.target.value
   try {
-    let res= await fetch(`https://api.chucknorris.io/jokes/random?category=${info}`)
+    let res= await fetch(`https://api.chucknorris.io/jokes/random?category=${category_name}`)
     let data= await res.json()
-    setJokes(data)
+    return(data)
   } catch (error) {
-    console.log(error);
+    return "something went wrong";;
   }
   // return the data ona successfull request
   // return error text on failure
