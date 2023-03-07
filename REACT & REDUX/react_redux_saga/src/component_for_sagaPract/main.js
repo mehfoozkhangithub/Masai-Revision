620abaa7b623cf1a4454483764ef7b73d99dd02b
unit-2_Program <100725778+mehfoozkhangithub@users.noreply.github.com>
2023-03-07 13:09:36 +0530

    code of all folder

--- REACT & REDUX/react_redux_saga/src/component_for_sagaPract/main.js
+++ REACT & REDUX/react_redux_saga/src/component_for_sagaPract/main.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { useDispatch } from "react-redux";
 import Header from "./header";
-import { AddToCart } from "../redux/action";
+import { AddToCart, removeFromChart } from "../redux/action";
 import "../App.css";
 function Main() {
   const dispatch = useDispatch(); // here we defined for the send data to action file.
 
-  //! this is stand for as a payload //
+  //! this is stand for as a payload data //
   const product = {
     name: "I Phone",
     price: "150K",
@@ -18,6 +18,12 @@ function Main() {
       <Header />
       {/* here we pass the data which we create upper name is product */}
       <button onClick={() => dispatch(AddToCart(product))}>Add To Cart</button>
+      <button onClick={() => dispatch(removeFromChart(product))}>
+        Remove To Cart
+      </button>
+      <button onClick={() => dispatch(deleteFromChart(product))}>
+        Empty To Cart
+      </button>
     </>
   );
 }
--- REACT & REDUX/react_redux_saga/src/redux/action.js
+++ REACT & REDUX/react_redux_saga/src/redux/action.js
@@ -1,9 +1,25 @@
-import { ADD_TO_CART } from "./type";
+import { ADD_TO_CART, REMOVE_FROM_CART } from "./type";
 
 export const AddToCart = (data) => {
   console.warn("action called");
   return {
     type: ADD_TO_CART,
+    data,
+  };
+};
+
+export const removeFromChart = (data) => {
+  console.warn("action called");
+  return {
+    type: REMOVE_FROM_CART,
+    data,
+  };
+};
+
+export const deleteFromChart = (data) => {
+  console.warn("action called");
+  return {
+    type: EMPTY_FROM_CART,
     data,
   };
 };
