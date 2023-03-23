f7baba2d869c235dc042f84596d842a708593c59
unit-2_Program <100725778+mehfoozkhangithub@users.noreply.github.com>
2023-03-23 10:23:14 +0530

    this is debounce code

---
+++ mock/Mock_explainer/Day-1/question_debounceing/Debouncer.js
@@ -0,0 +1,21 @@
+const getData = () => {
+  // calls an API and get the data;
+
+  console.log("fetching the data ...");
+};
+
+const doSomeMagic = (fu, delay) => {
+  let timer;
+  return function () {
+    let context = this;
+    args = arguments;
+    console.log("this is context output", context);
+    console.log("this is argument output", args);
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      getData.apply(context, arguments);
+    }, delay);
+  };
+};
+
+const betterFunction = doSomeMagic(getData, 300);
