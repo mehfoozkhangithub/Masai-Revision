0994db55aaf0edbfb2d3255fea728545b2afdd52
unit-2_Program <100725778+mehfoozkhangithub@users.noreply.github.com>
2023-03-26 06:52:20 +0530

    code

---
+++ mock/mockDSA/practiceForInterview.js
@@ -0,0 +1,46 @@
+// let Array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// let bag = "";
+// for (let i = Array.length - 1; i >= 0; i--) {
+//   bag += Array[i] + " ";
+// }
+
+// let ans = bag.split(" ").map(Number);
+
+// // console.log(ans);
+
+// let string = "Mehfooz Khan";
+
+// for (let i = 0; i < string.length; i++) {
+//   console.log(string[i]);
+// }
+
+// ******************** //
+
+let value = 19; //YES
+let num2 = 2; //NO
+
+let ans = value.toString();
+
+console.log(typeof ans);
+console.log(ans);
+
+// output was 1 and if that match we have to print YES or NO
+
+/* sum = 0;
+
+while (value) {
+  console.log("this is checking value", value);
+  sum += value % 10;
+  console.log("this is the sum after module operation", sum);
+  value = Math.floor(value / 10);
+}
+
+console.log("this for  checking what is in the real time value", value);
+console.log(sum);
+*/
+
+// ********************** //
+
+let arr1 = [1, 2, 0, 4, 5]; // 12
+
+let arr2 = [3, -4.1, 2, -1]; // 3
